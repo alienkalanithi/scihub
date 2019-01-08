7426712e76d45a70788fd57d8366763a808700c8
alienkalanithi <alienkalanithi@gmail.com>
2019-01-08 22:23:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+Sci-Hub-Fy
+==========
+
+Chrome extension that appends ".sci-hub.tw" to active tab domain, allowing free access to scientific articles.
+
+The hard work is done by [Sci-Hub].
+
+**Note:** The extension got removed from Chrome Store as they "don't allow products or services that facilitate unauthorized access to content on websites, such as circumventing paywalls or logins restrictions.". Of course you can just manually append the ".sci-hub.tw", but if you still want to automate this, the instructions below should make it work.
+
+## Installation
+
+You can load it as an unpacked extension in developer mode on Chrome. Follow this instructions:
+
+1. Clone this repo: `git clone https://github.com/allanino/sci-hub-fy.git`. Take note where you cloned it.
+2. Open Chrome and access `chrome://extensions`, or just open the menu -> settings -> extensions.
+3. Check the developer mode in upper right.
+4. Click "Load unpacked extension" button
+5. Select the folder into which you cloned **Sci-Hub-Fy** and click "Open".
+6. We're done. The extension should be available as if installed from Chrome store.
+
+## Context menu
+
+Another way of using this extension is through the context menu (right-click):
+
+- Link context: if you right-click a link and select Sci-Hub-Fy, we append ".sci-hub.tw" to the link and redirect you to it.
+- Page context: if you right-click anywhere but a link in a page, we append ".sci-hub.tw" to the page's URL and redirect you to it (the same as clicking in the extension icon).
+
+[Sci-Hub]:http://sci-hub.tw
---
+++ background.js
@@ -0,0 +1,69 @@
+// Copyright (c) 2014 Allan Costa.
+
+function sciHubFy(link, sciHubDomain) {
+  // Append Sci-Hub domain to link's domain
+  var matches = link.match(/:\/\/(?:www\.)?(.[^/]+)(.*)/);
+  return "https://" +sciHubDomain +"/"+ "http://" + matches[1] + matches[2]   ;
+  // Use the link to search on Sci-Hub
+  return "https://" + sciHubDomain + '/' + link;
+}
+
+function newTabSciHubFy(tab, link) {
+  // Tab is the current tab and link is the link to append sci-hub.io
+  chrome.storage.sync.get({
+    domain: 'sci-hub.tw' // Updated default domain ( As of DEC 03 - 2017 )
+  }, function(items) {
+    chrome.tabs.query({
+        active: true
+      }, tabs => {
+        let index = tabs[0].index;
+        chrome.tabs.create({index: index + 1, url: sciHubFy(link, items.domain)});
+      }
+    );
+  });
+}
+
+function sameTabSciHubFy(tab, link) {
+  // Tab is the current tab and link is the link to append sci-hub.io
+  chrome.storage.sync.get({
+    domain: 'sci-hub.tw' // Updated default domain ( As of DEC 03 - 2017 )
+  }, function(items) {
+    chrome.tabs.update(tab.id, {url: sciHubFy(link, items.domain)});
+  });
+}
+
+function openOptions(){
+  if (chrome.runtime.openOptionsPage) {
+  // New way to open options pages, if supported (Chrome 42+).
+  chrome.runtime.openOptionsPage();
+  } else {
+    // Reasonable fallback.
+    window.open(chrome.runtime.getURL('options.html'));
+  }
+}
+// Setup extension click action
+chrome.browserAction.onClicked.addListener(function(tab) {
+  sameTabSciHubFy(tab, tab.url);
+});
+
+// Setup context menu actions
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.contextMenus.create({
+        title: 'Sci-Hub-Fy',
+        id: 'page', // you'll use this in the handler function to identify this context menu item
+        contexts: ['page'],
+    });
+    chrome.contextMenus.create({
+        title: 'Sci-Hub-Fy',
+        id: 'link', // you'll use this in the handler function to identify this context menu item
+        contexts: ['link'],
+    });
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    if (info.menuItemId === "page") { // here's where you'll need the ID
+      sameTabSciHubFy(tab, tab.url);
+    } else if (info.menuItemId === "link") {
+      newTabSciHubFy(tab, info.linkUrl);
+    }
+});
